65d87c958979adc1b4aae3b2ddfda9bfafbae5fb
854731306@qq.com <854731306@qq.com>
2019-11-20 14:37:45 +0800

    SUN

--- src/main/java/com/demo/rest/HelloWorldController.java
+++ src/main/java/com/demo/rest/HelloWorldController.java
@@ -2,31 +2,61 @@ package com.demo.rest;
 
 import com.demo.model.SingleModel;
 import com.demo.service.GuiceDemo;
+import com.google.common.net.HttpHeaders;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.sun.deploy.net.HttpResponse;
+import org.apache.commons.io.IOUtils;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 /**
  * @author hewen
  * @date 2019/11/19 15:23
  */
 @Path("/hello")
-@Produces(value = MediaType.APPLICATION_JSON)
 public class HelloWorldController {
 
     @GET
     @Path("/get")
+    @Produces(value = MediaType.APPLICATION_JSON)
     public String helloWorld(@QueryParam("msg") String msg) {
         return "hello world!" + msg;
     }
 
     @POST
     @Path("/post")
+    @Produces(value = MediaType.APPLICATION_JSON)
     public Object helloWorldPost(@QueryParam("msg") String msg) {
         SingleModel build = SingleModel.builder().v(msg).build();
         return build;
+    }
+
+    @GET
+    @Path("/file")
+    public void exportFile(@Context HttpServletResponse response) throws Exception {
+        //得到输出流
+        ServletOutputStream outputStream = response.getOutputStream();
+        //设置下载文件头
+        String contentDisposition = "attachment;filename=test.txt";
+        response.addHeader(HttpHeaders.CONTENT_DISPOSITION, contentDisposition);
+        response.setContentType("application/vnd.ms-excel;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        //返回文件
+        for (int i = 0; i < 10000; i++) {
+            String str = UUID.randomUUID().toString()+"\n\t";
+            InputStream inputStream = getStringStream(str);
+            IOUtils.copy(inputStream,outputStream);
+        }
     }
 
 
@@ -34,5 +64,13 @@ public class HelloWorldController {
         Injector injector = Guice.createInjector();
         GuiceDemo helloGuice = injector.getInstance(GuiceDemo.class);
         helloGuice.say();
+    }
+
+    public InputStream getStringStream(String str) throws Exception{
+        if (str != null && !str.trim().equals("")) {
+            ByteArrayInputStream stringStream = new ByteArrayInputStream(str.getBytes());
+            return stringStream;
+        }
+        return null;
     }
 }
